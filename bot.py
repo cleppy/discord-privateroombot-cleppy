df6f8032d8d2af77ef73fe4e43d22091dc0eb42f
cleppy <111308587+cleppy@users.noreply.github.com>
2026-03-17 19:34:46 +0300

    Change bot command prefix from '!' to '.'

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ intents.message_content = True
 intents.voice_states = True
 intents.guilds = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 CATEGORY_NAME = "Private Rooms"
 
